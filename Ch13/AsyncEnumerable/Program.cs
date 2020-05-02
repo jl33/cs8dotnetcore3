38281d2840acd0856fa7ca2b1d1d7b6e5c3e5b10
Jason Liao <jason.liao.2005@gmail.com>
2020-05-02 20:49:01 +0800

    Ch13: add some new console applications

---
+++ Ch13/AsyncEnumerable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncEnumerable
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            await foreach(int number in GetNumbers()){
+                WriteLine($"Number: {number}");
+            }
+        }
+
+        static async IAsyncEnumerable<int> GetNumbers(){
+            var r =new Random();
+            System.Threading.Thread.Sleep(r.Next(1000,2000));
+            yield return r.Next(0,101);
+            System.Threading.Thread.Sleep(r.Next(1000,2000));
+            yield return r.Next(0,101);
+            System.Threading.Thread.Sleep(r.Next(1000,2000));
+            yield return r.Next(0,101);
+        }
+    }
+}
---
+++ Ch13/SynchornizingResourceAccess/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace SynchornizingResourceAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            WriteLine("Please wait for the tasks to complete.");
+            Stopwatch watch = Stopwatch.StartNew();
+            Task a = Task.Factory.StartNew(MethodA);
+            Task b = Task.Factory.StartNew(MethodB);
+            Task.WaitAll(new Task[] { a, b });
+            WriteLine("");
+            WriteLine($"Result: {Message}.");
+            WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed millisencods.");
+            WriteLine($"{Counter} string modifications.");
+        }
+
+        static Random r = new Random();
+        static string Message;
+        static object conch = new object();
+        static int Counter;
+
+        static void MethodA()
+        {
+            // lock (conch)
+            // {
+            //     for (int i = 0; i < 5; i++)
+            //     {
+            //         Thread.Sleep(r.Next(2000));
+            //         Message += "A";
+            //         Write(".");
+            //     }
+            // }
+            try
+            {
+                Monitor.TryEnter(conch,TimeSpan.FromSeconds(15));
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "A";
+                    Interlocked.Increment(ref Counter);
+                    Write(".");
+                }
+            }            
+            finally{
+                Monitor.Exit(conch);
+            }
+        }
+        static void MethodB()
+        {
+            // lock (conch)
+            // {
+            //     for (int i = 0; i < 5; i++)
+            //     {
+            //         Thread.Sleep(r.Next(2000));
+            //         Message += "B";
+            //         Write(".");
+            //     }
+            // }
+            try
+            {
+                // Monitor.Enter(conch);
+                Monitor.TryEnter(conch,TimeSpan.FromSeconds(15));
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "B";
+                    Interlocked.Increment(ref Counter);
+                    Write(".");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(conch);
+            }
+        }
+    }
+}
