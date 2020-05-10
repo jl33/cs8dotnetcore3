0aa4dc44e8fa265829841054878ed7220cb8f927
Jason Liao <jason.liao.2005@gmail.com>
2020-05-10 18:58:54 +0800

    Ch14:Entity practice

---
+++ Ch14/NorthwindEntitiesLib/Shipper.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+namespace Packt.Shared
+{
+    public class Shipper
+    {
+        public int ShipperID {get;set;}
+        public string ShipperName {get;set;}
+        public string Phone {get;set;}
+        public ICollection<Order> Orders {get;set;}
+    }
+}
